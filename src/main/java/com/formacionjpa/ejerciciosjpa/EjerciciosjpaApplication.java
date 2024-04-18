7920a2564cb000caeb3953c8bceb2f7789aee562
Pablo83G <pablovalverde83@gmail.com>
2024-04-18 10:18:45 +0200

    empezando jpa1

---
+++ src/main/java/com/formacionjpa/ejerciciosjpa/EjerciciosjpaApplication.java
@@ -0,0 +1,13 @@
+package com.formacionjpa.ejerciciosjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formacionjpa/ejerciciosjpa/EjerciciosjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionjpa.ejerciciosjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
